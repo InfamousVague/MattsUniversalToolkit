60e61488981d947f486067a58cfaec74e70f96a2
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-10-31 13:56:54 -0600

    update(ios): update identifier for ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from "@capacitor/cli"
 
 const config: CapacitorConfig = {
-    appId: "com.uplink.app",
+    appId: "im.satellite.core.mobile",
     appName: "Uplink",
     webDir: "build",
 }
