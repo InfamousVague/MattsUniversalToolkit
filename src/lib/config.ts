19f17055060a74c936c275db231b1dca9191f2f6
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2024-11-09 23:16:14 +0000

    chore(update): update 2.0.1 v2 (#840)

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -52,9 +52,9 @@ export const INTEGRATIONS = {
 }
 
 export const DOWNLOAD_LINKS = {
-  Android:  "https://github.com/Satellite-im/UplinkWeb/releases/download/v2.0.1/Uplink_2.0.1.apk",
+    Android:  "https://github.com/Satellite-im/UplinkWeb/releases/download/v2.0.1/Uplink_2.0.1.apk",
    // iOS: "https://github.com/Satellite-im/UplinkWeb/releases/download/v2.0.1/Uplink.ipa",
-    Windows: "https://github.com/Satellite-im/UplinkWeb/releases/download/v2.0.1/Uplink_2.0.1_x64_en-US.msi",
-    Mac: "https://github.com/Satellite-im/UplinkWeb/releases/download/v2.0.1/Uplink_2.0.1_aarch64.dmg",
-    Linux: "https://github.com/Satellite-im/UplinkWeb/releases/download/v2.0.1/Uplink_2.0.1_amd64.deb",
+    Windows: "https://github.com/Satellite-im/UplinkWeb/releases/download/v2.0.1/Uplink_2.0.1.msi",
+    Mac: "https://github.com/Satellite-im/UplinkWeb/releases/download/v2.0.1/Uplink_2.0.1.dmg",
+    Linux: "https://github.com/Satellite-im/UplinkWeb/releases/download/v2.0.1/Uplink_2.0.1.deb",
 }
