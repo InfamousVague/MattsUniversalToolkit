f186dfb87d3966b3dff9b9e7497fe8e8777a12b7
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-11-12 23:24:10 -0600

    update(ios): fix error when conflict solve

--- src/lib/components/ui/InstallBanner.svelte
+++ src/lib/components/ui/InstallBanner.svelte
@@ -10,11 +10,7 @@
         Windows = "Windows",
         MacOS = "MacOS",
         Android = "Android",
-<<<<<<< HEAD
         iOS = "iOS",
-=======
-        /* iOS = "iOS", */
->>>>>>> dev
         Linux = "Linux",
         Other = "Other",
     }
@@ -66,11 +62,7 @@
             icon: Shape.Android,
             download: DOWNLOAD_LINKS.Android,
         },
-<<<<<<< HEAD
         [Platform.iOS]: {
-=======
-        /*  [Platform.iOS]: {
->>>>>>> dev
             text: "iPhone",
             icon: Shape.Apple,
             download: DOWNLOAD_LINKS.iOS,
