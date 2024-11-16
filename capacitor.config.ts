2638fa3d5f74056c1f5560b29fac71292c8216a2
stavares843 <saratavares843@gmail.com>
2024-11-16 12:12:26 +0000

    remove

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,18 +4,6 @@ const config: CapacitorConfig = {
   appId: "com.satellite.uplink.app",
   appName: "Uplink",
   webDir: "build",
-
-  ios: {
-    // Set contentInset to "automatic" to handle safe areas
-    contentInset: "automatic",  // Ensures proper padding for status bar and bottom bar
-    cordova: {
-      preferences: {
-        StatusBarOverlaysWebView: "true",  // Ensures status bar doesn't push content
-        KeyboardResizeMode: "ionic",       // Prevents keyboard from resizing content
-        UIScrollViewContentInsetAdjustmentBehavior: "automatic",  // Automatically adjusts the scroll view content inset
-      }
-    }
-  }
 };
 
 export default config;
