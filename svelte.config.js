140bb777e932d6f502c8b883cbab660f4ce7ff4b
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2024-11-20 10:33:34 +0000

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
-import adapter from '@sveltejs/adapter-static';
-import path from 'node:path';
+import adapter from "@sveltejs/adapter-static"
+import path from "node:path"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
