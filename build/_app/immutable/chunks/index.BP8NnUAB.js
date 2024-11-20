522096276d4318c67e0f869a6d6bc47c6fffe043
stavares843 <stavares843@users.noreply.github.com>
2024-11-20 11:17:17 +0000

    deploy: 985f00488986c05eb6b39a00592183ca82e626c8

---
+++ build/_app/immutable/chunks/index.BP8NnUAB.js
@@ -0,0 +1,1 @@
+import{_ as i}from"./preload-helper.C1FmrZbK.js";import{r as a,i as e,g as o,l as t}from"./Store.Cvio1nxY.js";a("en",()=>i(()=>import("./en.C2FjNTG3.js"),[],import.meta.url));async function _(){a("en",()=>i(()=>import("./en.C2FjNTG3.js"),[],import.meta.url)),await e({fallbackLocale:"en",initialLocale:o()}),t.debug("Locale initialized")}export{_ as i};
---
+++ build/_app/immutable/entry/start.CczhZzMj.js
@@ -0,0 +1,1 @@
+import{c as a}from"../chunks/entry.pBd951-U.js";export{a as start};
