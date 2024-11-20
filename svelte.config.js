3b3e82308c53351b271d0189e5e69908f910799b
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2024-11-20 10:56:44 +0000

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
-import adapter from "@sveltejs/adapter-static"
-import path from "node:path"
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-static";
+import path from "node:path";
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-preprocess: [vitePreprocess({})],
+  preprocess: [vitePreprocess({})], 
   kit: {
     paths: {
-      base: '/UplinkWeb', // Ensure this is set for GitHub Pages deployment
+      base: '/UplinkWeb', 
     },
     adapter: adapter({
-      fallback: '404.html', // Necessary for single-page apps to work correctly
+      fallback: '404.html', 
     }),
+    appDir: 'build/_app',
   },
 };
 
