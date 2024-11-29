4f79244c975a2b5444031fffccdbaddedb6d1777
lgmarchi <lucasgmarchi@gmail.com>
2024-11-29 10:17:11 -0300

    fix(Mobile): Add a correct check to show or not keybinds section

--- src/lib/utils/Mobile.ts
+++ src/lib/utils/Mobile.ts
@@ -1,3 +1,10 @@
+import { Device } from "@capacitor/device"
+
 export function checkMobile(): boolean {
     return window.matchMedia("screen and (max-width: 800px)").matches
+}
+
+export async function isAndroidOriOS(): Promise<boolean> {
+    const info = await Device.getInfo()
+    return info.platform === "ios" || info.platform === "android"
 }
