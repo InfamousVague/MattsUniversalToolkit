bc65c1654bc06a053df64c327d0bdd37475726a3
lgmarchi <lucasgmarchi@gmail.com>
2024-12-04 19:15:35 -0300

    fix(layout): enhance device orientation handling by adding isAndroidOriOS check

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,7 +28,7 @@
     import Market from "$lib/components/market/Market.svelte"
     import { swipe } from "$lib/components/ui/Swipe"
     import { ScreenOrientation } from "@capacitor/screen-orientation"
-    import { fetchDeviceInfo } from "$lib/utils/Mobile"
+    import { fetchDeviceInfo, isAndroidOriOS } from "$lib/utils/Mobile"
 
     log.debug("Initializing app, layout routes page.")
 
@@ -276,7 +276,8 @@
     }
 
     onMount(async () => {
-        if (await fetchDeviceInfo()) {
+        await fetchDeviceInfo()
+        if (await isAndroidOriOS()) {
             lockOrientation()
         }
 
