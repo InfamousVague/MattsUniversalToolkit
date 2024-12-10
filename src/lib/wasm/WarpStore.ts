9de6b2eee89ce138b2cd976c3fc383b31391548d
Flemmli97 <Flemmli97@users.noreply.github.com>
2024-12-10 18:00:47 +0100

    update wasm and remote export

--- src/lib/wasm/WarpStore.ts
+++ src/lib/wasm/WarpStore.ts
@@ -67,6 +67,8 @@ class Store {
         } else {
             config = wasm.Config.minimal_basic()
         }
+        // Shuttle address is temporary! For testing!
+        config.enable_shuttle_discovery(["/ip4/138.197.62.183/tcp/7008/ws/p2p/12D3KooW9tAgS6kZPmEtfRiBHsYC46dN2hv6hT6gQ8bqxzWw7y5X"])
         config.set_save_phrase(get(AuthStore.state).saveSeedPhrase)
         config.set_thumbnail_size(500, 500)
         config.with_thumbnail_exact_format(true)
